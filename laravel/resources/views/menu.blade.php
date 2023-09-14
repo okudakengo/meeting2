588f2d5426fddf5cb5ff4b0d4ccf77e7fb1b4a2f
Kengo Okuda <okuda@msdnet.jp>
2023-09-14 09:31:13 +0900

    add Menu Bar

--- laravel/resources/views/menu.blade.php
+++ laravel/resources/views/menu.blade.php
@@ -1,17 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-    	
-    	<!-- left menu -->
-    	@include('menu')
-    	
-        <!--<div class="col-md-8">-->
-        <div class="col-md-10">
+        <div class="col-md-2">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">メニュー</div>
+				
+				<!--
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -21,8 +12,12 @@
 
                     You are logged in!
                 </div>
+                -->
+                
+                <nav id='sidemenu'>
+	                <a href='/home'>ミーティング 管理</a>
+					<a href='/users'>ユーザー 管理</a>
+				</nav>
+                
             </div>
         </div>
-    </div>
-</div>
-@endsection
