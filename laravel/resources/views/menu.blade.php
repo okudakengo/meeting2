2c9b70b1beb1f496e1e075c1611d5e04c47e25bc
okudakengo <okuda@msdnet.jp>
2023-09-14 13:06:46 +0900

    modify indent

--- laravel/resources/views/menu.blade.php
+++ laravel/resources/views/menu.blade.php
@@ -1,23 +1,23 @@
         <div class="col-md-2">
             <div class="card">
                 <div class="card-header">メニュー</div>
-				
-				<!--
+								
+								<!--
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
+										
                     You are logged in!
                 </div>
                 -->
                 
                 <nav id='sidemenu'>
 	                <a href='/home'>ミーティング 管理</a>
-					<a href='/users'>ユーザー 管理</a>
-				</nav>
+									<a href='/users'>ユーザー 管理</a>
+								</nav>
                 
             </div>
         </div>
